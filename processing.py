e3b2bc93bad01ce7d2f725076d4b4773fc06766a
Matheus Bressan <46140177+math-exe@users.noreply.github.com>
2023-09-05 09:59:59 -0300

    Create processing.py

---
+++ processing.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+
+def date_generator():
+    today = datetime.now()
+    delta = timedelta(days=15)
+    year = today.year
+
+    start_date = datetime(year, 1, 1)
+    
+    while start_date <= today:
+        finish_date = start_date + delta
+        if finish_date > today:
+            finish_date = today
+
+        yield start_date.strftime('%Y-%m-%d'), finish_date.strftime('%Y-%m-%d')
+        start_date = finish_date + timedelta(days=1)
+
+def processing_data(dados_api):
+    processing_data = []
+    for item in dados_api:
+        # Processamento específico
+        processing_data.append({
+            'chaveAcesso' : item['chaveAcesso'],
+            'status' : item['status'],
+            'nomeEmitente' : item['nomeEmitente'],
+            'cnpjEmitente' : item['cnpjEmitente'],
+            'nomeDestinatario' : item['nomeDestinatario'],
+            'cnpjDestinatario' : item['cnpjDestinatario'],
+            'dataEmissao' : item['dataEmissao'],
+            'numero' : item['numero'],
+            'serie' : item['serie'],
+            'valor' : item['valor'],
+            'baseIcms' : item['baseIcms'],
+            'valorIcms' : item['valorIcms'],
+            'situacaoNota' : item['situacaoNota']
+        })
+    return processing_data
